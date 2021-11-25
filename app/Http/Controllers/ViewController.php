f74374feceb6f0541ef64b4f3a199ad56f4daff0
MasAbdurrachmanHanan <92034882+MasAbdurrachmanHanan@users.noreply.github.com>
2021-11-25 19:57:10 +0700

    Update ViewController.php

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -18,4 +18,20 @@ class ViewController extends Controller
         //code untuk meload data yang akan dimuat di form
         return view('tugas');
     }
+
+    function  isiForm()
+    {
+        return view('tugas6html');
+    }
+
+    function resultHasil ()
+    {
+        return view('tugas6php');
+    }
+
+    function  etsForm()
+    {
+        return view('ets');
+    }
+
 }
