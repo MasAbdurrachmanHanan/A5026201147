460a36087c7a57682630ab218b78d8a01243b772
MasAbdurrachmanHanan <92034882+MasAbdurrachmanHanan@users.noreply.github.com>
2021-11-25 19:56:45 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,20 @@ Route::get('praktikum2', function () {
 //jika ternyata butuh ngeload banyak proses
 Route::get('isiannama', "ViewController@showForm") ;
 Route::post('greetings', "ViewController@resultGreetings") ;
+Route::get('isibmi', "ViewController@isiForm") ;
+Route::post('hasilbmi', "ViewController@resultHasil") ;
+Route::get('ets', "ViewController@etsForm") ;
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+//route CRUD pendapatan
+Route::get('/pendapatan','PendapatanController@index');
+Route::get('/pendapatan/tambah','PendapatanController@tambah');
+Route::post('/pendapatan/store','PendapatanController@store');
+Route::get('/pendapatan/edit/{ID}','PendapatanController@edit');
+Route::post('/pendapatan/update','PendapatanController@update');
+Route::get('/pendapatan/hapus/{ID}','PendapatanController@hapus');
